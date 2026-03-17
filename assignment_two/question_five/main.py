3aca9393c83f71e731b509a6bf540414fe77f5c8
rudaperfect <mukukurudaviro@gmail.com>
2026-03-17 14:33:00 +0200

    created  folder  for  assignment  two  file

---
+++ assignment_two/question_five/main.py
@@ -0,0 +1,9 @@
+import math_operations
+
+# Using the add function
+result = math_operations.add(5, 7)
+print("Sum:", result)
+
+# Using the multiply function
+product = math_operations.multiply(5, 7)
+print("Product:", product)
